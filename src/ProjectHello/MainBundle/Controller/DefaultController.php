6765d8aaca4e17a49455acda153b9444b658181f
Neri Celeste <nherrisa.celeste@gmail.com>
2012-06-20 21:28:36 +0800

    initial commit of project structure; added main bundle

---
+++ src/ProjectHello/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ProjectHello\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('ProjectHelloMainBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/ProjectHello/MainBundle/ProjectHelloMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProjectHello\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProjectHelloMainBundle extends Bundle
+{
+}
