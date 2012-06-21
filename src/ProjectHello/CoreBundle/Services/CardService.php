0e5bcf6594ba03d9ef0d9bea25c65978fbf805bb
Neri Celeste <nherrisa.celeste@gmail.com>
2012-06-22 00:23:46 +0800

    added services

---
+++ src/ProjectHello/CoreBundle/Services/CardService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProjectHello\CoreBundle\Services;
+
+use ProjectHello\CoreBundle\Entity\User;
+
+class CardService
+{
+	public function retrieveCardsCreatedByProponent(User $proponent)
+	{
+
+	}
+
+	public function retrieveCardsReceivedByRecipient(User $recipient)
+	{
+
+	}
+
+	public function retrieveCardsWithMessagesWrittenByAuthor(MessageAuthor $author)
+	{
+
+	}
+
+	public function retrieveCardsOfType($type)
+	{
+		
+	}
+}
---
+++ src/ProjectHello/CoreBundle/Services/MessageAuthorService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ProjectHello\CoreBundle\Services;
+
+use ProjectHello\CoreBundle\Entity\Card;
+use ProjectHello\CoreBundle\Entity\Invitation;
+
+class MessageAuthorService
+{
+	public function retrieveAuthorsWhoWroteMessagesForTheCard(Card $card)
+	{
+
+	}
+
+	public function retrieveAuthorsWhoDidNotConfirmAnInvitation(Invitation $invitation)
+	{
+
+	}
+}
