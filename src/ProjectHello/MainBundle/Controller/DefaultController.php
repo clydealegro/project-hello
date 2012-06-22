a6c80beec7142b385e896092cbc5eda1327be817
Neri Celeste <nherrisa.celeste@gmail.com>
2012-06-22 14:44:46 +0800

    added messages data

--- src/ProjectHello/MainBundle/Controller/DefaultController.php
+++ src/ProjectHello/MainBundle/Controller/DefaultController.php
@@ -18,6 +18,17 @@ class DefaultController extends Controller
 
     public function viewCardAction()
     {
+        $messages = array(
+            array(
+                'author'    => 'Farly & Czar',
+                'message'   => "\"Hi Mon, Every code has its origin, more importantly programming is a journey thus moving on is part of it.. Goodluck in your new career pursuit.\"\n\nWeb you around :D"
+            ),
+            array(
+                'author'    => 'Ronald Ryan L. Vy',
+                'message'   => "hi mon!\n\tGoodluck and best wishes!!!keep on rocking MONster!!!"
+            )
+        );
+
         return $this->render('ProjectHelloMainBundle:Default:view_card.html.twig');
     }
 }
